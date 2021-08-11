cacd269f9d8641b51d38e9aeb656271bc67ebb48
Beau Dobbin <beau@beaunus.com>
2021-08-11 21:09:19 +0900

    refactor: convert Outer to functional component

--- src/App.js
+++ src/App.js
@@ -25,37 +25,32 @@ const Bottom = () => {
   return <div>Bottom</div>;
 };
 
-class Outer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { showTop: true, showBottom: true };
-  }
+const Outer = () => {
+  const [state, setState] = React.useState({ showTop: true, showBottom: true });
 
-  render() {
-    return (
-      <>
-        <input
-          type="checkbox"
-          checked={this.state.showTop}
-          onChange={(event) =>
-            this.setState({ ...this.state, showTop: event.target.checked })
-          }
-        />
-        Show Top
-        <input
-          type="checkbox"
-          checked={this.state.showBottom}
-          onChange={(event) =>
-            this.setState({ ...this.state, showBottom: event.target.checked })
-          }
-        />
-        Show Bottom
-        {this.state.showTop ? <Top /> : null}
-        {this.state.showBottom ? <Bottom /> : null}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <input
+        type="checkbox"
+        checked={state.showTop}
+        onChange={(event) =>
+          setState({ ...state, showTop: event.target.checked })
+        }
+      />
+      Show Top
+      <input
+        type="checkbox"
+        checked={state.showBottom}
+        onChange={(event) =>
+          setState({ ...state, showBottom: event.target.checked })
+        }
+      />
+      Show Bottom
+      {state.showTop ? <Top /> : null}
+      {state.showBottom ? <Bottom /> : null}
+    </>
+  );
+};
 
 function App() {
   return <Outer />;
